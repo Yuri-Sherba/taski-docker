cae82c3d15012617d0665c1985def81d30fe349b
Юра Щерба <goodexon@yandex.ru>
2025-06-10 15:03:31 +0300

    Fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.172.148']
+ALLOWED_HOSTS = ['89.169.172.148', 'https://taskiyuri.zapto.org']
 
 
 # Application definition
